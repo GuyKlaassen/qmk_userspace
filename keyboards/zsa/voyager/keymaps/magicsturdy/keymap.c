cbb0934a46de034dc2593b1494c1c1bd34cc8772
GuyKlaassen <klaassen.guy@gmail.com>
2025-05-14 12:20:35 +0200

    New SYM layer layout

--- keyboards/zsa/voyager/keymaps/magicsturdy/keymap.c
+++ keyboards/zsa/voyager/keymaps/magicsturdy/keymap.c
@@ -71,17 +71,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TAB , BSPCSYM
   ),
 
-  [SYM] = LAYOUT_LR(  // Base layer: Magic Sturdy.
-    _______, KC_F1  , KC_F2  , KC_F3  ,  KC_F4 , KC_F5  ,
-    _______, KC_1   , KC_2   , KC_3   ,  KC_4  , KC_5   ,
-    _______, KC_EXLM, KC_AT  , KC_HASH,  KC_DLR, KC_PERC,
-    _______, _______, _______, _______, _______, _______,
+    [SYM] = LAYOUT_LR(  // Base layer: Magic Sturdy.
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
+    _______, _______, KC_7   , KC_8   , KC_9   , _______,
+    _______, KC_0   , KC_1   , KC_2   , KC_3   , _______,
+    _______, _______, KC_4   , KC_5   , KC_6   , _______,
                                                  _______, _______,
 
                       KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11,
-                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_F12,
-                      KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,
-                      _______, _______, _______, _______, _______, _______,
+                      _______, C_MAGIC, KC_HASH, KC_DLR , _______, KC_F12,
+                      _______, KC_EXLM, KC_AT  , KC_PERC, _______, _______,
+                      _______, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______,
              _______, _______
   ),
 

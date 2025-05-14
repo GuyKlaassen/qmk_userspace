9f9a2580ce119d4854c1c6917a7037d618ff6075
GuyKlaassen <klaassen.guy@gmail.com>
2025-05-14 10:47:01 +0200

    Exclude one handed shortcuts

--- keyboards/zsa/voyager/keymaps/magicsturdy/keymap.c
+++ keyboards/zsa/voyager/keymaps/magicsturdy/keymap.c
@@ -110,14 +110,14 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
   '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
   '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-  '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-                                               'L'    , 'L'    ,
+  '*'    , '*'    , 'L'    , 'L'    , 'L'    , 'L'    ,
+                                               '*'    , '*'    ,
 
                     '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
                     'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
                     'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
                     'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
-           'R'    , 'R'
+           '*'    , '*'
 );
 #endif  // CHORDAL_HOLD
 
